a97a5d96ac590b5c4c48e37b1261436bc23ed173
chengkangzai <pycck@hotmail.com>
2022-03-14 00:08:53 +0800

    Done With Facade
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- src/com/company/facade/AbstractFacade.java
+++ src/com/company/facade/AbstractFacade.java
@@ -1,10 +1,11 @@
 package com.company.facade;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractFacade<T> {
 
-    public List<T> list;
+    public List<T> list = new ArrayList<>();
 
     public List<T> get() {
         return this.list;
